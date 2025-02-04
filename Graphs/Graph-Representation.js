9c75328ff3dfeedd778f708f2d4cec189eec94f7
Manisha Patil <manishapatil030@gmail.com>
2025-02-04 10:45:36 +0530

    Refactor graph representation to use utility functions for adding edges and printing adjacency list

--- Graphs/Graph-Representation.js
+++ Graphs/Graph-Representation.js
@@ -1,55 +1,32 @@
-class Graph {
-    constructor() {
-        this.adjacencyList = {};
-    }
+// A simple representation of graph using JavaScript
 
-    addVertex(vertex) {
-        if (!this.adjacencyList[vertex]) {
-            this.adjacencyList[vertex] = [];
-        }
-    }
+// A utility function to add an edge in an undirected graph.
+function addEdge(adj, u, v) {
+    adj[u].push(v);
+    adj[v].push(u);
+}
 
-    addEdge(vertex1, vertex2) {
-        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-            this.adjacencyList[vertex1].push(vertex2);
-            this.adjacencyList[vertex2].push(vertex1);
+// A utility function to print the adjacency list representation of graph
+function printGraph(adj, V) {
+    for (let v = 0; v < V; ++v) {
+        let output = `\n Adjacency list of vertex ${v}\n head `;
+        for (let x of adj[v]) {
+            output += `-> ${x}`;
         }
-    }
-
-    removeEdge(vertex1, vertex2) {
-        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-                v => v !== vertex2
-            );
-            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-                v => v !== vertex1
-            );
-        }
-    }
-
-    removeVertex(vertex) {
-        if (this.adjacencyList[vertex]) {
-            while (this.adjacencyList[vertex].length) {
-                const adjacentVertex = this.adjacencyList[vertex].pop();
-                this.removeEdge(vertex, adjacentVertex);
-            }
-            delete this.adjacencyList[vertex];
-        }
-    }
-
-    display() {
-        for (let vertex in this.adjacencyList) {
-            console.log(vertex + " -> " + this.adjacencyList[vertex].join(", "));
-        }
+        console.log(output);
     }
 }
 
-// Example usage:
-const graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addEdge("A", "B");
-graph.addEdge("A", "C");
-graph.addEdge("B", "C");
-graph.display();
+// Driver code
+const V = 5;
+const adj = Array.from({ length: V }, () => []);
+addEdge(adj, 0, 1);
+addEdge(adj, 0, 4);
+addEdge(adj, 1, 2);
+addEdge(adj, 1, 3);
+addEdge(adj, 1, 4);
+addEdge(adj, 2, 3);
+addEdge(adj, 3, 4);
+printGraph(adj, V);
+
+    
